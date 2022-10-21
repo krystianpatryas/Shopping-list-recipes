ab58dc7dc5393c12bb8ca6bbf83cb8101ea9398b
Krystian Patryas <krystian.patryas@signify.com>
2022-10-21 10:34:25 +0200

    auth logging

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  isLoading = false;
   isLoginMode = true;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -21,18 +24,25 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
-    if(this.isLoginMode) {
-      //...
+    let authObs: Observable<AuthResponseData>
+
+    this.isLoading = true;
+      if(this.isLoginMode) {
+     authObs = this.authService.login(email,password)
     } else {
-
-            this.authService.signUp(email,password).subscribe(res =>{
-               console.log(res)
-              },
-              err => {
-                console.log(err)
-              });
-
+      authObs = this.authService.signUp(email,password)
     }
+    authObs.subscribe(
+      res => {
+        console.log(res);
+        this.isLoading = false;
+      },
+      err => {
+        console.log(err);
+        this.error = err;
+        this.isLoading = false;
+      });
+
     form.reset();
   }
 }
